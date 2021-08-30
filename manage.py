b2b1e7c2067b819360285b8847c2104c949d5a75
Joël Franusic <joel@franusic.com>
2021-08-30 11:41:57 -0700

    Format using Black

--- manage.py
+++ manage.py
@@ -6,8 +6,8 @@ from app import app
 migrate = Migrate(app, db)
 manager = Manager(app)
 
-manager.add_command('db', MigrateCommand)
+manager.add_command("db", MigrateCommand)
 
 
-if __name__ == '__main__':
-	manager.run()
+if __name__ == "__main__":
+    manager.run()
