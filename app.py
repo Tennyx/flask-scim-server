4fb89e56d3054622ce6b04555167fed2aef14166
Oktenny <cale.switzer@okta.com>
2021-08-31 16:16:46 -0400

    added config to get rid of deprecation warning in SQLALCHEMY,

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ def create_app():
     """
     app = Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://localhost/scim"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     return app
 
